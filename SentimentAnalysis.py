5be51c392a42f64ec650c9d2c069860f9a5f1aaf
Navaneeth Sudheer <navneet.sudheer@gmail.com>
2018-03-31 00:49:57 -0400

    Added the file

---
+++ SentimentAnalysis.py
@@ -0,0 +1,20 @@
+import tweepy
+from textblob import TextBlob
+
+consumer_key = 	'00IVGVqeBASnvUsqhujUmghmT'
+consumer_secret = 'OBLRHcfTaBQp1LhW0nr0Tdo93Vt6ODcq2TMkX5CPGqZIeOeo1M'
+
+access_token = '344622805-2YWt81TbNUkNlzmXsR10fevWiRc3MgZus5Ntz09W'
+access_token_secret = 'UfSwqrLy182G01ZX1L3Qo6Uk0HWLnhLbVZPWtGO1smFaz'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('bitcoin')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
